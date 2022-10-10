cd40764668c22668de3c3d997613f22f985518e5
Konstantyn <k.buhantsev@gmail.com>
2022-10-10 22:06:37 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { customAlphabet } from 'nanoid';
 import { Formik } from 'formik';
@@ -15,54 +15,52 @@ const phoneRegExp =
 const nameReExp = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
 
 const schema = Yup.object().shape({
-  name: Yup.string().min(3).matches(nameReExp, 'Name is not valid').required(),
+  name: Yup.string()
+    .min(3, 'Minimum 3 letters!')
+    .matches(nameReExp, 'Name is not valid!')
+    .required('This field is required!'),
   number: Yup.string()
-    .matches(phoneRegExp, 'Phone number is not valid')
-    .max(13)
-    .required(),
+    .matches(phoneRegExp, 'Phone number is not valid!')
+    .max(13, 'Maximum 13 numbers!')
+    .required('This field is required!'),
 });
 
-class ContactForm extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func,
-  };
-
-  handleSubmit = (values, { resetForm }) => {
+function ContactForm({ onSubmit }) {
+  const handleSubmit = (values, { resetForm }) => {
     const { name, number } = values;
     const nanoid = customAlphabet('1234567890', 10);
     const id = 'id-' + nanoid(2);
-    const { onSubmit } = this.props;
 
     if (onSubmit({ id, name, number })) {
       resetForm();
     }
   };
 
-  render() {
-    return (
-      <Formik
-        initialValues={{ name: '', number: '' }}
-        validationSchema={schema}
-        onSubmit={this.handleSubmit}
-      >
-        <FormStyled name="contact-form">
-          <label htmlFor="name">
-            Name:
-            <FieldStyled type="text" name="name" />
-            <ErrorMessageStyled name="name" component="div" />
-          </label>
+  return (
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
+      <FormStyled name="contact-form">
+        <label htmlFor="name">
+          Name:
+          <FieldStyled type="text" name="name" />
+          <ErrorMessageStyled name="name" component="div" />
+        </label>
 
-          <label htmlFor="number">
-            Number:
-            <FieldStyled type="tel" name="number" />
-            <ErrorMessageStyled name="number" component="div" />
-          </label>
+        <label htmlFor="number">
+          Number:
+          <FieldStyled type="tel" name="number" />
+          <ErrorMessageStyled name="number" component="div" />
+        </label>
 
-          <ButtonStyled type="submit">Add contact</ButtonStyled>
-        </FormStyled>
-      </Formik>
-    );
-  }
+        <ButtonStyled type="submit">Add contact</ButtonStyled>
+      </FormStyled>
+    </Formik>
+  );
 }
+
+ContactForm.tropTypes = { onSubmit: PropTypes.func };
 
 export default ContactForm;
